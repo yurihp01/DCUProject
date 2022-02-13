f94341ae74470809b0e40d2b7ce1c066255a4518
Yuri <yurihp01@hotmail.com>
2022-02-13 01:09:47 +0000

    criado o projeto e algumas telas

---
+++ IHCProject/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  IHCProject
+//
+//  Created by PRO on 12/02/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ IHCProjectUITests/IHCProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  IHCProjectUITestsLaunchTests.swift
+//  IHCProjectUITests
+//
+//  Created by PRO on 12/02/2022.
+//
+
+import XCTest
+
+class IHCProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
