05d6e83ad7d506e2a282fb81ee4d7a7714b09c05
Yuri <yurihp01@hotmail.com>
2022-02-19 15:36:25 +0000

    add BaseViewController and Utils

---
+++ DCUProject/Base/BaseViewController.swift
@@ -0,0 +1,36 @@
+//
+//  BaseViewController.swift
+//  DCUProject
+//
+//  Created by Yuri Pedroso on 19/02/22.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController, Storyboarded {
+    
+    // MARK: Variables
+    lazy var indicator: UIActivityIndicatorView = {
+        let percent = view.bounds.height - view.bounds.height * 0.2
+        let indicator = UIActivityIndicatorView(frame: CGRect(x: view.center.x - 24, y: percent, width: 40, height: 40))
+        indicator.accessibilityIdentifier = Constants.indicator
+        indicator.color = .white
+        indicator.style = .large
+        indicator.hidesWhenStopped = true
+        return indicator
+    } ()
+    
+    // MARK: - Override Functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(indicator)
+    }
+    
+    // MARK: - Functions
+    func showAlert(message: String?) {
+        guard let message = message else { return }
+        let alert = UIAlertController.showAlertDialog(title: Constants.titleError, message: message)
+        self.present(alert, animated: true, completion: nil)
+    }
+}
---
+++ DCUProject/Utils/UIAlertController+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  .swift
+//  DCUProject
+//
+//  Created by Yuri Pedroso on 19/02/22.
+//
+
+import UIKit
+
+extension UIAlertController {
+    // MARK: - Functions
+    static func showAlertDialog(title: String, message: String) -> UIAlertController {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: Constants.okButton, style: .default, handler: nil)
+        alert.addAction(okAction)
+        return alert
+    }
+}
