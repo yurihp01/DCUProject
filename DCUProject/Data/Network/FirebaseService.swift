d05182f5c18f0fd1142c3c1e1803cb95b80f93ae
Yuri <yurihp01@hotmail.com>
2022-02-19 16:17:36 +0000

    add login to firebase

--- DCUProject/Data/Network/FirebaseService.swift
+++ DCUProject/Data/Network/FirebaseService.swift
@@ -12,16 +12,24 @@ typealias Handle = AuthStateDidChangeListenerHandle
 protocol FirebaseServiceProtocol: AnyObject {
     var handle: Handle { get }
     func removeHandle(handle: Handle)
+    func login(email: String, password: String) async throws -> String
 }
 
-class FirebaseService { }
-
-extension FirebaseService: FirebaseServiceProtocol {
+class FirebaseService: FirebaseServiceProtocol {
     var handle: Handle {
         return Auth.auth().addStateDidChangeListener { auth, user in }
     }
     
     func removeHandle(handle: Handle) {
         Auth.auth().removeStateDidChangeListener(handle)
+    }
+    
+    func login(email: String, password: String) async throws -> String {
+        do {
+            try await Auth.auth().signIn(withEmail: email, password: password)
+            return "Logado com sucesso!"
+        } catch {
+            return error.localizedDescription
+        }
     }
 }
