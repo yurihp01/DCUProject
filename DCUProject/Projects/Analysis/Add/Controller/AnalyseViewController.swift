71b2a0eedfadca6e526ed23fcac689acc3b24a4e
Yuri <yurihp01@hotmail.com>
2022-03-01 18:32:12 +0000

    restructuring folders

---
+++ DCUProject/Projects/Analysis/Add/Controller/AnalyseViewController.swift
@@ -0,0 +1,49 @@
+////
+////  ProjectsViewController.swift
+////  DCUProject
+////
+////  Created by PRO on 20/02/2022.
+////
+//
+//import UIKit
+//
+//class ProjectsViewController: BaseViewController {
+//    weak var coordinator: ProjectsCoordinator?
+//    var viewModel: ProjectsViewModelProtocol?
+//    @IBOutlet weak var tableView: UITableView!
+//    @IBOutlet weak var searchBar: UISearchBar!
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+//        tableView.delegate = self
+//        tableView.dataSource = self
+//        searchBar.delegate = self
+//        // Do any additional setup after loading the view.
+//    }
+//}
+//
+//extension ProjectsViewController: UITableViewDelegate, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        guard let projects = viewModel?.getProjects(by: searchBar.text),
+//                projects.count > 0 else { return UITableViewCell() }
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        cell.textLabel?.text = projects[indexPath.row].name
+//        return cell
+//    }
+//    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        guard let project = viewModel?.getProjects(by: searchBar.text)[indexPath.row] else { return }
+//        coordinator?.goToCompletedProject(with: project)
+//    }
+//    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return viewModel?.getProjects(by: searchBar.text) .count ?? 0
+//    }
+//}
+//
+//extension ProjectsViewController: UISearchBarDelegate {
+//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        tableView.reloadData()
+//    }
+//}
---
+++ DCUProject/Projects/Analysis/Add/Coordinator/AnalyseCoordinator.swift
@@ -0,0 +1,33 @@
+////
+////  AnalyseCoordinator.swift
+////  DCUProject
+////
+////  Created by PRO on 20/02/2022.
+////
+//
+//import UIKit
+//
+//class AnalyseCoordinator: Coordinator {
+//    var navigationController: UINavigationController
+//    
+//    var childCoordinators: [Coordinator] = []
+//    
+//    var parentCoordinator: Coordinator?
+//    
+//    init (with navigationController: UINavigationController) {
+//        self.navigationController = navigationController
+//    }
+//    
+//    func start() {
+//        let viewController = ProjectsViewController.instantiate(storyboardName: .main)
+//        viewController.viewModel = ProjectsViewModel()
+//        viewController.coordinator = self
+//        navigationController.pushViewController(viewController, animated: true)
+//    }
+//    
+//    func goToCompletedProject(with project: Project) {
+//        let coordinator = HomeCoordinator(navigationController: navigationController, project: project)
+//        add(coordinator)
+//        coordinator.start()
+//    }
+//}
