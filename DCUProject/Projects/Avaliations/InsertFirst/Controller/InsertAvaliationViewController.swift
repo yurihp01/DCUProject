9df81eb0945d337ad8b419faccbf79703210a3e4
Yuri Pedroso <yurihp01@hotmail.com>
2022-05-04 02:43:35 +0100

    add Floating field and fixes bugs

--- DCUProject/Projects/Avaliations/InsertFirst/Controller/InsertAvaliationViewController.swift
+++ DCUProject/Projects/Avaliations/InsertFirst/Controller/InsertAvaliationViewController.swift
@@ -5,19 +5,27 @@
 //  Created by PRO on 05/03/2022.
 //
 
-import iOSDropDown
 import UIKit
+
+class CellClass: UITableViewCell {}
 
 class InsertAvaliationViewController: BaseViewController {
     
-    @IBOutlet weak var screen: DropDown!
-    @IBOutlet weak var heuristic: DropDown!
+    @IBOutlet weak var screen: FloatingLabelField!
+    @IBOutlet weak var heuristic: FloatingLabelField!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var stackStatus: UIStackView!
     
-    @IBOutlet weak var status: DropDown!
-    @IBOutlet weak var severity: DropDown!
+    @IBOutlet weak var status: FloatingLabelField!
+    @IBOutlet weak var severity: FloatingLabelField!
     @IBOutlet weak var button: BorderedButton!
     @IBOutlet weak var avaliator: FloatingLabelField!
     @IBOutlet weak var titleField: FloatingLabelField!
+    
+    let transparentView = UIView()
+    let tableView = UITableView()
+    var dataSource = [String]()
+    var selectedField = FloatingLabelField()
     
     weak var coordinator: InsertAvaliationCoordinator?
     var viewModel: InsertAvaliationViewModel?
@@ -59,20 +67,44 @@ class InsertAvaliationViewController: BaseViewController {
 }
 
 private extension InsertAvaliationViewController {
-    func setDropdowns() {
-        guard let project = viewModel?.project else { return }
-        screen.optionArray = project.preAvaliation.screens
-        heuristic.optionArray = project.preAvaliation.heuristics
-        status.optionArray = ["Sucesso", "Defeito"]
-        severity.optionArray = [Severity.cosmetic.rawValue, Severity.lower.rawValue, Severity.serious.rawValue, Severity.disaster.rawValue]
-        severity.isEnabled = status.text == "Defeito"
-        setDropdownSelections()
-    }
+    func addTransparentView(frames: CGRect) {
+         transparentView.frame = self.view.frame
+         self.view.addSubview(transparentView)
+         
+        tableView.frame = CGRect(x: stackView.frame.minX, y: frames.origin.y + stackView.frame.minY, width: stackView.frame.width, height: CGFloat(dataSource.count * 50))
+         self.view.addSubview(tableView)
+         tableView.layer.cornerRadius = 5
+         
+         transparentView.backgroundColor = UIColor.black.withAlphaComponent(0.9)
+         tableView.reloadData()
+         let tapgesture = UITapGestureRecognizer(target: self, action: #selector(removeTransparentView))
+         transparentView.addGestureRecognizer(tapgesture)
+             self.transparentView.alpha = 0.5
+     }
+        
+    @objc func removeTransparentView() {
+         let frames = selectedField.frame
+         self.transparentView.alpha = 0
+         self.tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height, width: frames.width, height: 0)
+     }
     
-    func setDropdownSelections() {
-        status.didSelect{(selectedText , index ,id) in
-            self.severity.isEnabled = selectedText == "Defeito"
-        }
+    func setDropdowns() {
+        screen.addTarget(self, action: #selector(myTargetFunction), for: .touchDown)
+        heuristic.addTarget(self, action: #selector(myTargetFunction), for: .touchDown)
+        status.addTarget(self, action: #selector(myTargetFunction), for: .touchDown)
+        severity.addTarget(self, action: #selector(myTargetFunction), for: .touchDown)
+        severity.isEnabled = status.text == "Defeito"
+        screen.image.isHidden = false
+        screen.delegate = self
+        heuristic.delegate = self
+        status.delegate = self
+        severity.delegate = self
+        heuristic.image.isHidden = false
+        status.image.isHidden = false
+        severity.image.isHidden = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(CellClass.self, forCellReuseIdentifier: "Cell")
     }
         
     func setFields() {
@@ -86,5 +118,62 @@ private extension InsertAvaliationViewController {
             severity.text = avaliation.status == "Sucesso" ? "" : avaliation.status
         }
         title = buttonType.rawValue
+    }
+    
+    @objc func myTargetFunction(textField: FloatingLabelField) {
+        guard let project = viewModel?.project else { return }
+        switch textField {
+        case screen:
+            dataSource = project.preAvaliation.screens
+            selectedField = textField
+            addTransparentView(frames: textField.frame)
+        case heuristic:
+            dataSource = project.preAvaliation.heuristics
+            selectedField = textField
+            addTransparentView(frames: textField.frame)
+        case status:
+            dataSource = ["Sucesso", "Defeito"]
+            selectedField = textField
+            addTransparentView(frames: stackStatus.frame)
+        case severity:
+            dataSource = [Severity.cosmetic.rawValue, Severity.lower.rawValue, Severity.serious.rawValue, Severity.disaster.rawValue]
+            selectedField = textField
+            addTransparentView(frames: stackStatus.frame)
+        default:
+            break
+        }
+    }
+}
+
+extension InsertAvaliationViewController: UITableViewDelegate, UITableViewDataSource {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return dataSource.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+            cell.textLabel?.text = dataSource[indexPath.row]
+            return cell
+        }
+        
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            if selectedField == status {
+                severity.isEnabled = dataSource[indexPath.row] == "Defeito"
+                severity.text = dataSource[indexPath.row] == "Defeito"  ? severity.text : ""
+            }
+            selectedField.text = dataSource[indexPath.row]
+            removeTransparentView()
+        }
+    }
+
+extension InsertAvaliationViewController: UITextFieldDelegate {
+
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        switch textField {
+        case screen, heuristic, status, severity:
+            return false
+        default:
+            return true
+        }
     }
 }
--- DCUProject/Utils/Custom/FloatingLabelField.swift
+++ DCUProject/Utils/Custom/FloatingLabelField.swift
@@ -11,11 +11,24 @@ class FloatingLabelField: UITextField {
     var floatingLabel: UILabel = UILabel(frame: CGRect.zero) // Label
     var floatingLabelHeight: CGFloat = 14 // Default height
     
+    lazy var image: UIImageView = {
+        var image = UIImageView(frame: CGRect(x: 0, y: 0, width: 16, height: 16))
+        image.image = UIImage(systemName: "arrowtriangle.down.fill")
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.isHidden = true
+        addSubview(image)
+        image.rightAnchor.constraint(equalTo:
+        self.rightAnchor, constant: -16).isActive = true
+        image.centerYAnchor.constraint(equalTo:
+        self.centerYAnchor).isActive = true
+        return image
+    }()
+    
     @IBInspectable
     var _placeholder: String? // we cannot override 'placeholder'
     
     @IBInspectable
-    var floatingLabelColor: UIColor = UIColor.black {
+    var floatingLabelColor: UIColor = UIColor.darkGray {
         didSet {
             self.floatingLabel.textColor = floatingLabelColor
             self.setNeedsDisplay()
@@ -33,29 +46,43 @@ class FloatingLabelField: UITextField {
         }
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+    override var text: String? {
+        didSet {
+            self.floatingLabel.text = text == "" ? "" : placeholder
+        }
+    }
+    
+    func setvalues() {
         self._placeholder = (self._placeholder != nil) ? self._placeholder : placeholder // Use our custom placeholder if none is set
         placeholder = self._placeholder // make sure the placeholder is shown
         self.floatingLabel = UILabel(frame: CGRect.zero)
         backgroundColor = .white
-        self.addTarget(self, action: #selector(self.addFloatingLabel), for: .editingDidBegin)
-        self.addTarget(self, action: #selector(self.removeFloatingLabel), for: .editingDidEnd)
+        self.floatingLabel.textColor = floatingLabelColor
+        self.floatingLabel.font = floatingLabelFont
+        self.floatingLabel.text = text == "" ? "" : placeholder
+        self.floatingLabel.layer.backgroundColor = UIColor.white.cgColor
+        self.floatingLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.floatingLabel.clipsToBounds = true
+        delegate = self
+        self.floatingLabel.frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.floatingLabelHeight)
+        self.addSubview(self.floatingLabel)
+        floatingLabel.bottomAnchor.constraint(equalTo:
+        self.topAnchor, constant: 0).isActive = true
+        
+        
+    }
+    
+    init (image: UIImage = UIImage()) {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setvalues()
     }
     
     @objc func addFloatingLabel() {
         if self.text == "" {
-            self.floatingLabel.textColor = floatingLabelColor
-            self.floatingLabel.font = floatingLabelFont
-            self.floatingLabel.text = self._placeholder
-            self.floatingLabel.layer.backgroundColor = UIColor.white.cgColor
-            self.floatingLabel.translatesAutoresizingMaskIntoConstraints = false
-            self.floatingLabel.clipsToBounds = true
-            self.floatingLabel.frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.floatingLabelHeight)
-            self.addSubview(self.floatingLabel)
-          
-            self.floatingLabel.bottomAnchor.constraint(equalTo:
-            self.topAnchor, constant: 0).isActive = true
             self.placeholder = ""
         }
         self.setNeedsDisplay()
@@ -67,4 +94,24 @@ class FloatingLabelField: UITextField {
             placeholder = self._placeholder
         }
     }
+}
+
+extension FloatingLabelField: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if string == "" {
+            if let text = textField.text,
+               text.trimmingCharacters(in: .whitespaces).count <= 1 {
+                floatingLabel.text = nil
+                placeholder = self._placeholder
+            } else {
+                
+            }
+        } else {
+            floatingLabel.text = placeholder
+            
+        }
+        return true
+    }
+    
 }
